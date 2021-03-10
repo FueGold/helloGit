13e1f5a22e9f1949b45e199a0ab3cd8d196e5dae
FueGold <1911136370@qq.com>
2021-03-10 16:29:13 +0800

    测试第一次提交Git

---
+++ src/main/java/com/atguigu/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+/**
+ * @ClassName: HelloGit
+ * @Description: TODO
+ * @Author: Fa
+ * @Date: 2021-03-10 16:09
+ * @Version: V1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
